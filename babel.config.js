1abeaa76fbbd167818fb701bf679fb10b5739995
Giulia Borges da Silva <giuliaborgesdasilva@gmail.com>
2024-10-17 11:06:09 -0300

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-    api.cache(true)
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [['module:react-native-dotenv']],
-    }
-}
-// Arquivo de configuração do projeto
-// Esse plugin é uma referência ao arquivo do .env
